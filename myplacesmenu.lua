563a16ab8a8ff57eb3044afcd8bf931a3c6387ce
Arjen Dijksta <dijkstra.arjen@gmail.com>
2011-11-13 12:55:51 +0100

    add files

---
+++ myplacesmenu.lua
@@ -0,0 +1,37 @@
+module("myplacesmenu")
+
+myplacesmenu = {}
+myplacesmenu[1] = {
+{[====[file_tree_me]====], [====[geany '/home/arjend/binfile_tree_menu.py']====] },
+{[====[jython]====], [====[leafpad '/home/arjend/binjython']====] },
+{[====[fortune_linu]====], [====[leafpad '/home/arjend/binfortune_linuxkernel']====] },
+{[====[syncroSVNCli]====], [====[geany '/home/arjend/binsyncroSVNClient-64bit.sh']====] },
+{[====[diffFiles]====], [====[leafpad '/home/arjend/bindiffFiles']====] },
+{[====[run-pida.py]====], [====[geany '/home/arjend/binrun-pida.py']====] },
+{[====[pypy]====], [====[leafpad '/home/arjend/binpypy']====] },
+{[====[cc.py]====], [====[geany '/home/arjend/bincc.py']====] },
+{[====[syncroSVNCli]====], [====[leafpad '/home/arjend/binsyncroSVNClient']====] },
+{[====[nasaBackgrou]====], [====[geany '/home/arjend/binnasaBackground.sh']====] },
+{[====[fortune_drug]====], [====[leafpad '/home/arjend/binfortune_drugs']====] },
+{[====[dreampie]====], [====[leafpad '/home/arjend/bindreampie']====] },
+{[====[diffDirs]====], [====[leafpad '/home/arjend/bindiffDirs']====] },
+{[====[fortune_linu]====], [====[leafpad '/home/arjend/binfortune_linux']====] },
+{[====[pygenie.py]====], [====[geany '/home/arjend/binpygenie.py']====] },
+{[====[QtWebCache]====], [====[leafpad '/home/arjend/binQtWebCache']====] },
+{[====[boa.sh]====], [====[geany '/home/arjend/binboa.sh']====] },
+{[====[osfortune]====], [====[leafpad '/home/arjend/binosfortune']====] },
+{[====[QtWebSetting]====], [====[leafpad '/home/arjend/binQtWebSettings']====] },
+{[====[terminator]====], [====[leafpad '/home/arjend/binterminator']====] },
+{[====[bofh]====], [====[leafpad '/home/arjend/binbofh']====] },
+{[====[ff]====], [====[leafpad '/home/arjend/binff']====] },
+{[====[webkitsimple]====], [====[geany '/home/arjend/binwebkitsimple.py']====] },
+{[====[QtWeb]====], [====[leafpad '/home/arjend/binQtWeb']====] },
+{[====[ps_mem.py]====], [====[geany '/home/arjend/binps_mem.py']====] },
+{[====[git-gui]====], [====[leafpad '/home/arjend/bingit-gui']====] },
+{[====[cc.pyc]====], [====[leafpad '/home/arjend/bincc.pyc']====] },
+{'open here', 'nautilus ' .. [====['/home/arjend/bin']====]}
+ }
+
+
+passed = myplacesmenu[1]
+function myplacesmenu() return passed end
